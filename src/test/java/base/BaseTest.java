ed923f45f5ab841169494efca0eb93bcd966577c
Raiyan Ali <raiyanaliofficial@gmail.com>
2026-09-03 21:58:39 +0530

    Configure Chrome for CI execution

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,11 +4,15 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 import utils.ConfigReader;
+import utils.TestListener;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     protected WebDriver driver;
@@ -20,14 +24,22 @@ public class BaseTest {
         String baseUrl = ConfigReader.getProperty("baseUrl");
 
         if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
+
         } else {
+
             throw new RuntimeException(
                     "Unsupported browser: " + browser
             );
         }
-
-        driver.manage().window().maximize();
 
         driver.manage().timeouts().implicitlyWait(
                 Duration.ofSeconds(5)
